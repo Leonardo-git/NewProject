7a9ea4cc84d92104d5c1066e4f46c8d4f1315fc8
Leonardo <barbosasleonardo@gmail.com>
2021-01-21 08:54:17 -0300

    Inicializacao

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import '../styles/pages/landing.css'
+
+function Landing(){
+  return(
+    <React.Fragment>
+      <header>
+        
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+      <div>
+        <h1>Landing Page</h1>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Landing
